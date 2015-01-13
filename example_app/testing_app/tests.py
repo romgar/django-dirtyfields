cb7997fe38d22a6610dea6c0e4f9b20cbca7f98b
hobbestigrou <natal.ngetal@novapost.fr>
2015-01-13 09:50:48 +0100

    [Core] Be pep8 compliant.

--- example_app/testing_app/tests.py
+++ example_app/testing_app/tests.py
@@ -1,20 +1,14 @@
-from django.core.management.color import no_style
-from django.db import models
 from django.test import TestCase
-from django.conf import settings
-from django.core.management import call_command
-from django.db.models.loading import load_app
-from django.db import connection, transaction
 
 from example_app.testing_app.models import TestModel
 
+
 class DirtyFieldsMixinTestCase(TestCase):
-    
     def test_dirty_fields(self):
         tm = TestModel()
         # initial state shouldn't be dirty
         self.assertEqual(tm.get_dirty_fields(), {})
-        
+
         # changing values should flag them as dirty
         tm.boolean = False
         tm.characters = 'testing'
@@ -22,13 +16,13 @@ class DirtyFieldsMixinTestCase(TestCase):
             'boolean': True,
             'characters': ''
         })
-        
+
         # resetting them to original values should unflag
         tm.boolean = True
         self.assertEqual(tm.get_dirty_fields(), {
             'characters': ''
         })
-    
+
     def test_sweeping(self):
         tm = TestModel()
         tm.boolean = False
@@ -39,4 +33,3 @@ class DirtyFieldsMixinTestCase(TestCase):
         })
         tm.save()
         self.assertEqual(tm.get_dirty_fields(), {})
-    
