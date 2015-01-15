dba12b8301aff78fcbeffeb794c5751c8a954d33
hobbestigrou <natal.ngetal@novapost.fr>
2015-01-15 09:30:01 +0100

    [Core] Optimize the code replace dict and use iteritems (fixes #14).

--- src/dirtyfields/dirtyfields.py
+++ src/dirtyfields/dirtyfields.py
@@ -6,17 +6,30 @@ from django.db.models.signals import post_save
 class DirtyFieldsMixin(object):
     def __init__(self, *args, **kwargs):
         super(DirtyFieldsMixin, self).__init__(*args, **kwargs)
-        post_save.connect(reset_state, sender=self.__class__,
-                        dispatch_uid='%s-DirtyFieldsMixin-sweeper' % self.__class__.__name__)
+        post_save.connect(
+            reset_state, sender=self.__class__,
+            dispatch_uid='{name}-DirtyFieldsMixin-sweeper'.format(
+                name=self.__class__.__name__))
         reset_state(sender=self.__class__, instance=self)
 
     def _as_dict(self):
-        return dict([(f.name, getattr(self, f.name)) for f in self._meta.local_fields if not f.rel])
+        all_field = {}
+
+        for field in self._meta.local_fields:
+            if not field.rel:
+                all_field[field.name] = getattr(self, field.name)
+
+        return all_field
 
     def get_dirty_fields(self):
         new_state = self._as_dict()
+        all_modify_field = {}
 
-        return dict([(key, value) for key, value in self._original_state.items() if value != new_state[key]])
+        for key, value in self._original_state.iteritems():
+            if value != new_state[key]:
+                all_modify_field[key] = value
+
+        return all_modify_field
 
     def is_dirty(self):
         # in order to be dirty we need to have been saved at least once, so we
