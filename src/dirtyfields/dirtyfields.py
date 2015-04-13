6f88c9ddc3afdd3701218da1f368bcc3aa8648b2
Hernan Lozano <hernantz@gmail.com>
2015-04-12 22:47:51 -0300

    Implement getter methods to allow extending dirtyfields to obtain special/custom field values

--- src/dirtyfields/dirtyfields.py
+++ src/dirtyfields/dirtyfields.py
@@ -19,16 +19,28 @@ class DirtyFieldsMixin(object):
         for field in self._meta.local_fields:
             if field.rel and not check_relationship:
                 continue
-            try:
+            getter = self._get_dirtyfield_getter(field)
+            if getter:
+                all_field[field.name] = getattr(self, getter)(field)
+            else:
                 all_field[field.name] = getattr(self, field.name)
-            except ObjectDoesNotExist:
-                all_field[field.name] = None
 
         return all_field
 
+    def _get_dirtyfield_getter(self, field):
+        cls = field.__class__
+        for klass in (cls,) + cls.__bases__:
+            getter_name = 'get_{}_dirtyfield'.format(klass.__name__)
+            if hasattr(self, getter_name):
+                break
+        else:
+            getter_name = False
+
+        return getter_name
+
     def get_dirty_fields(self, check_relationship=False):
-        # check_relationship indicates whether we want to check for foreign keys
-        # and one-to-one fields or ignore them
+        # check_relationship indicates whether we want to check for
+        # foreign keys and one-to-one fields or ignore them
         new_state = self._as_dict(check_relationship)
         all_modify_field = {}
 
