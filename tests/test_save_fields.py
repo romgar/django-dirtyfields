41dca9760ba58e4c7e29d86d4b238c6ea55d6802
romgar <romain.garrigues.cs@gmail.com>
2015-07-02 22:47:35 +0100

    Removing assert on query counts, that need to be more specific on testing

--- tests/test_save_fields.py
+++ tests/test_save_fields.py
@@ -8,9 +8,7 @@ def test_save_specific_field():
     tm = TestModel.objects.create()
 
     tm.characters = 'new_character'
-    with assert_number_queries(2):
-        tm.save()
+    tm.save()
 
     tm.characters = 'new_character_2'
-    with assert_number_queries(1):
-        tm.save_dirty_fields()
+    tm.save_dirty_fields()
