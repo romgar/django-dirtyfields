e3e97c049616eb3e90c17c459a97e3aa6e8bc4b0
romgar <romain.garrigues.cs@gmail.com>
2015-09-27 18:27:23 +0100

    Adding test to check that we are not updating every field when using save_dirty_fields method

--- tests/compat.py
+++ tests/compat.py
@@ -1,3 +1,6 @@
+import django
+import unittest
+
 
 def get_model_name(klass):
     if hasattr(klass._meta, 'model_name'):
@@ -6,3 +9,9 @@ def get_model_name(klass):
         model_name = klass._meta.module_name
 
     return model_name
+
+
+def skip_before_django_15(test):
+    if django.VERSION < (1, 5):
+        return unittest.skip('save dirty fields is not different that basic save() before django 1.5')(test)
+    return test
--- tests/test_save_fields.py
+++ tests/test_save_fields.py
@@ -1,14 +1,27 @@
 import pytest
+
 from .models import TestModel
-from .utils import assert_number_queries
+from tests.compat import skip_before_django_15
+from .utils import assert_number_of_queries_on_regex
 
 
+@skip_before_django_15
 @pytest.mark.django_db
-def test_save_specific_field():
+def test_save_dirty_simple_field():
     tm = TestModel.objects.create()
 
     tm.characters = 'new_character'
-    tm.save()
+
+    with assert_number_of_queries_on_regex(r'.*characters.*', 1):
+        with assert_number_of_queries_on_regex(r'.*boolean.*', 1):
+            tm.save()
+
+    assert TestModel.objects.get(pk=tm.pk).characters == 'new_character'
 
     tm.characters = 'new_character_2'
-    tm.save_dirty_fields()
+
+    with assert_number_of_queries_on_regex(r'.*characters.*', 1):
+        with assert_number_of_queries_on_regex(r'.*boolean.*', 0):
+            tm.save_dirty_fields()
+
+    assert TestModel.objects.get(pk=tm.pk).characters == 'new_character_2'
