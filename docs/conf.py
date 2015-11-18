0932f2a1adb5220c3e5a44ebb52991cb0ec8ab40
romgar <romain.garrigues.cs@gmail.com>
2015-11-18 20:12:02 +0000

    Removing space in project name

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'django dirtyfields'
+project = u'django-dirtyfields'
 copyright = u'2015, Romain Garrigues'
 author = u'Romain Garrigues'
 
