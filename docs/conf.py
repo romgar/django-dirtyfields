c849f24ee79b5b7c0730a49796ced43a5ebb91b0
romgar <romain.garrigues.cs@gmail.com>
2015-11-22 19:08:18 +0000

    Adding intersphinx configuration for python/sphinx/django

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,16 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.intersphinx'
+]
+
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/2.7', None),
+    'sphinx': ('http://sphinx.pocoo.org/', None),
+    'django': ('http://docs.djangoproject.com/en/dev/',
+               'http://docs.djangoproject.com/en/dev/_objects/'),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
