cb1f030ff845fa02d0d49cc92f4f54243e141f47
romgar <romain.garrigues.cs@gmail.com>
2016-06-25 20:49:05 +0100

    Add compatibility for deprecated old _meta API

--- src/dirtyfields/compat.py
+++ src/dirtyfields/compat.py
@@ -4,6 +4,17 @@ from django.db.models import signals
 from django.db.models.query_utils import DeferredAttribute
 
 
+def get_m2m_with_model(given_model):
+    if django.VERSION < (1, 9):
+        return given_model._meta.get_m2m_with_model()
+    else:
+        return [
+            (f, f.model if f.model != given_model else None)
+            for f in given_model._meta.get_fields()
+            if f.many_to_many and not f.auto_created
+        ]
+
+
 def is_db_expression(value):
     try:
         # django < 1.8
@@ -44,7 +55,7 @@ def save_specific_fields(instance, fields_list):
 
 
 def is_buffer(value):
-    if sys.version_info < (3,0,0):
+    if sys.version_info < (3, 0, 0):
         return isinstance(value, buffer)
     else:
         return isinstance(value, memoryview)
