27e032d3443d965d99232db4dfcfa0374653ac63
romgar <romain.garrigues.cs@gmail.com>
2016-08-11 22:28:25 +0100

    Add a new postgresql section in tox configuration + update settings usage with py.test to easily change if needed

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-
-# Needed for django tests
-os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.django_settings'
-if django.VERSION >= (1, 7, 0):
-    # setup() call is needed for django 1.7+
-    django.setup()
