8824b09de15189d87a5f91f6b21cf27aafff12e3
romgar <romain.garrigues.cs@gmail.com>
2016-08-16 21:36:51 +0100

    Update travis script and postgresql settings to work on travis env

--- tests/postgresql_django_settings.py
+++ tests/postgresql_django_settings.py
@@ -4,12 +4,12 @@ SECRET_KEY = 'WE DONT CARE ABOUT IT'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'xxx',                      # Or path to database file if using sqlite3.
-        'USER': 'xxx',                      # Not used with sqlite3.
-        'PASSWORD': 'xxx',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dirtyfields_test',  # Should be the same defined in .travis.yml
+        'USER': 'postgres',  # postgres user is by default created in travis-ci
+        'PASSWORD': '',  # postgres user has no password on travis-ci
+        'HOST': 'localhost',
+        'PORT': '5432',  # default postgresql port
     }
 }
 
