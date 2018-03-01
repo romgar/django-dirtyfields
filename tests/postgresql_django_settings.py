4e1720ef502d33382cdf22ca3e18af6f0b8624ba
Hugo Smett <hsmett@hybird.org>
2018-03-01 10:37:00 +0100

    Allow to define postgres settings with environment variables

--- tests/postgresql_django_settings.py
+++ tests/postgresql_django_settings.py
@@ -1,13 +1,16 @@
+import os
+
 # Minimum files that are needed to run django test suite
+
 
 SECRET_KEY = 'WE DONT CARE ABOUT IT'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dirtyfields_test',  # Should be the same defined in .travis.yml
-        'USER': 'postgres',  # postgres user is by default created in travis-ci
-        'PASSWORD': '',  # postgres user has no password on travis-ci
+        'NAME': os.getenv('POSTGRES_NAME', 'dirtyfields_test'),  # Should be the same defined in .travis.yml
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),  # postgres user is by default created in travis-ci
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),  # postgres user has no password on travis-ci
         'HOST': 'localhost',
         'PORT': '5432',  # default postgresql port
     }
