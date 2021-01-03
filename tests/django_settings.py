06679c93911786c10d86ac2e397dda236017d9a9
Lincoln Puzey <lincoln@puzey.dev>
2021-01-03 16:46:10 +0800

    Run all tests jobs with postgresql, and test sqlite with a single job.
    Because postgresql is more frequently used in production.
    Also collect coverage report from a regular job, rather than run an extra job for coverage.
    All this makes tox.ini much simpler. Also able to combine django settings into one file.

--- tests/django_settings.py
+++ tests/django_settings.py
@@ -1,16 +1,28 @@
-# Minimum files that are needed to run django test suite
+# Minimum settings that are needed to run django test suite
+import os
 
 SECRET_KEY = 'WE DONT CARE ABOUT IT'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dirtyfields.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+if "postgresql" in os.getenv("TOX_ENV_NAME", ""):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            # Should be the same defined in .travis.yml
+            'NAME': 'dirtyfields_test',
+            # postgres user is by default created in travis-ci
+            'USER': os.getenv('POSTGRES_USER', 'postgres'),
+            # postgres user has no password on travis-ci
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': 'localhost',
+            'PORT': '5432',  # default postgresql port
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'dirtyfields.db',
+        }
+    }
 
 INSTALLED_APPS = ('tests', )
