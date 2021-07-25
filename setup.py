ebe6cb6765625f7cdfb3254042e37f324a3d4c5c
Lincoln Puzey <lincoln@puzey.dev>
2021-07-25 17:48:23 +0800

    Remove setup.py, not required since travis doesn't build anymore.

--- setup.py
+++
@@ -1,7 +0,0 @@
-# All package metadata is in setup.cfg
-# This is still required because travis builds with "python setup.py"
-
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
