734e109720fba67c2b0f9a3cd300c9e53883d416
Lincoln Puzey <lincoln@puzey.dev>
2022-01-23 13:22:06 +0800

    Add custom links to docs sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -167,7 +167,15 @@ html_static_path = ['_static']
 # html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
+html_sidebars = {
+    '**': [
+        'about.html',
+        'dirtyfields-links.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
