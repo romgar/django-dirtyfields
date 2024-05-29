200e90b6bb73d7e918f9d589da0cc8b1f996ef72
Lincoln Puzey <18750802+LincolnPuzey@users.noreply.github.com>
2024-05-29 20:34:34 +0800

    start next version

--- src/dirtyfields/__init__.py
+++ src/dirtyfields/__init__.py
@@ -5,7 +5,7 @@ Adapted from https://stackoverflow.com/questions/110803/dirty-fields-in-django
 """
 
 __all__ = ['DirtyFieldsMixin']
-__version__ = "1.9.3"
+__version__ = "1.9.4.dev0"
 from dirtyfields.dirtyfields import DirtyFieldsMixin
 
 VERSION = tuple(map(int, __version__.split(".")[0:3]))
