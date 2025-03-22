1a540e9341a3923af8f8eeb87d4a2caa2ea8cc30
LincolnPuzey <18750802+LincolnPuzey@users.noreply.github.com>
2025-03-22 17:39:20 +0800

    Fix `Model.refresh_from_db()` so calling it with 3 positional args works. (#239)

--- tests/test_core.py
+++ tests/test_core.py
@@ -215,6 +215,19 @@ def test_refresh_from_db_no_fields():
     assert tm.get_dirty_fields() == {"boolean": True, "characters": "old value"}
 
 
+@pytest.mark.django_db
+def test_refresh_from_db_position_args():
+    tm = ModelTest.objects.create(characters="old value")
+    tm.boolean = False
+    tm.characters = "new value"
+    assert tm.get_dirty_fields() == {"boolean": True, "characters": "old value"}
+
+    tm.refresh_from_db("default", {"boolean", "characters"})
+    assert tm.boolean is True
+    assert tm.characters == "old value"
+    assert tm.get_dirty_fields() == {}
+
+
 @pytest.mark.skipif(django.VERSION < (5, 1), reason="requires django 5.1 or higher")
 @pytest.mark.django_db
 def test_refresh_from_db_with_from_queryset():
@@ -228,6 +241,20 @@ def test_refresh_from_db_with_from_queryset():
     assert tm.get_dirty_fields() == {"boolean": True, "characters": "old value"}
 
     tm.refresh_from_db(fields={"characters"}, from_queryset=ModelTest.objects.all())
+    assert tm.boolean is False
+    assert tm.characters == "old value"
+    assert tm.get_dirty_fields() == {"boolean": True}
+
+
+@pytest.mark.skipif(django.VERSION < (5, 1), reason="requires django 5.1 or higher")
+@pytest.mark.django_db
+def test_refresh_from_db_position_args_with_queryset():
+    tm = ModelTest.objects.create(characters="old value")
+    tm.boolean = False
+    tm.characters = "new value"
+    assert tm.get_dirty_fields() == {"boolean": True, "characters": "old value"}
+
+    tm.refresh_from_db("default", {"characters"}, ModelTest.objects.all())
     assert tm.boolean is False
     assert tm.characters == "old value"
     assert tm.get_dirty_fields() == {"boolean": True}
